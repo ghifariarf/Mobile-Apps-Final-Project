d744e80d7aebc9742cd92685e9e05776ffa30386
ghifari <ghifariarfananda76@gmail.com>
2019-12-22 20:42:47 +0700

    Tinggal generate semua jalur

---
+++ app/src/main/java/com/example/tamapbox/data.java
@@ -0,0 +1,19 @@
+package com.example.tamapbox;
+
+public class data {
+    String rute;
+    int bobot;
+
+    public data(String rute, int bobot) {
+        this.rute = rute;
+        this.bobot = bobot;
+    }
+
+    public String getRute() {
+        return rute;
+    }
+
+    public int getBobot() {
+        return bobot;
+    }
+}
